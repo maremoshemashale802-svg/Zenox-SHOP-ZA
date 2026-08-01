72030d6e3f5e78920b42c5f6604379e6b2eaf1be
maremoshemashale802-svg <maremoshemashale802@gmail.com>
2026-08-01 17:09:21 +0200

    Add product routes to server middleware

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/auth", authRoutes);
+app.use("/products", productRoutes);
 // Serve static files
 app.use("/public", express.static(path.join(__dirname, "public")));
 
