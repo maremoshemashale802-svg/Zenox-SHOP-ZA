3e19cccb6f72f5baebf3378f025f3544a6c4440e
maremoshemashale802-svg <maremoshemashale802@gmail.com>
2026-08-01 11:06:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const authRoutes = require("./routes/authRoutes");
 const db = require("./config/db");
+require("dotenv").config();
 
 const app = express();
 const PORT = 3000;
